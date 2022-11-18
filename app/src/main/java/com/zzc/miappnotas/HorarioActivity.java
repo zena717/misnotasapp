40d9977940fd9136df1a3c0ecc47dc8b7f34f6da
Jose Juan <jjmoraherrera@gmail.com>
2022-11-17 23:03:53 -0500

    Projecto Inicial

---
+++ app/src/main/java/com/zzc/miappnotas/HorarioActivity.java
@@ -0,0 +1,14 @@
+package com.zzc.miappnotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HorarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horario);
+    }
+}
---
+++ app/src/main/java/com/zzc/miappnotas/IngresarActivity.java
@@ -0,0 +1,14 @@
+package com.zzc.miappnotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IngresarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingresar);
+    }
+}
